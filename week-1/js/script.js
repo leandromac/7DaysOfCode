ae9f709934a80456a7d30f0d8bd116b066966dc2
leandromac <contato@leandromacedo.com>
2020-09-14 16:28:16 -0300

    Include a task in my ToDo

--- week-1/js/script.js
+++ week-1/js/script.js
@@ -1,1 +1,18 @@
-alert('hellow')
+
+  const input = document.querySelector('input')
+  const buttonAdd = document.querySelector('.button')
+  const list = document.querySelector('.list')
+
+  buttonAdd.addEventListener('click', e => {
+    let a = document.createElement('a')
+    a.className = "panel-block is-active"
+    a.innerHTML = `<span class="panel-icon">
+                    <i class="fas fa-book" aria-hidden="true"></i>
+                  </span>
+                  ${input.value}`
+  list.appendChild(a)
+  input.value = ''
+  })
+
+
+            
