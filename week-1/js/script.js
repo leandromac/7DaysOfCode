4065eaa2d8475389bccaf0f6af052d750a9955e9
leandromac <contato@leandromacedo.com>
2020-09-14 17:01:30 -0300

    Refatoring

--- week-1/js/script.js
+++ week-1/js/script.js
@@ -4,14 +4,20 @@
   const list = document.querySelector('.list')
 
   buttonAdd.addEventListener('click', e => {
-    let a = document.createElement('a')
-    a.className = "panel-block is-active"
-    a.innerHTML = `<span class="panel-icon">
-                    <i class="fas fa-book" aria-hidden="true"></i>
-                  </span>
-                  ${input.value}`
-  list.appendChild(a)
-  input.value = ''
+    if(!!input.value === false) {
+      alert('You need to type a task!')
+    }
+    else {
+      let a = document.createElement('a')
+      a.className = "panel-block task is-active"
+      a.innerHTML = `<span class="panel-icon">
+                      <i class="fas fa-book" aria-hidden="true"></i>
+                    </span>
+                    ${input.value}`
+      a.setAttribute('onclick', 'taskDone(this)')
+    list.appendChild(a)
+    input.value = ''
+    }
   })
 
 
