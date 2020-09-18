1cda2c074f560094e277a6699b69f73b28668694
leandromac <contato@leandromacedo.com>
2020-09-18 17:06:04 -0300

    Create button open url function

--- Day-5/js/script.js
+++ Day-5/js/script.js
@@ -4,15 +4,24 @@
   const btn = document.querySelector('.btn')
   const btnDelete = document.querySelector('[aria-label="delete"]')
   const panelUrl = document.querySelector('.message')
+  const btnCopy = document.querySelector('.btn-copy')
+  const btnOpen = document.querySelector('.btn-open')
 
   btn.addEventListener('click', e => {
     e.preventDefault()
     urlGenerate()
+
   })
 
-  btnDelete.addEventListener('click', () => {
+  btnDelete.addEventListener('click', e => {
+    e.preventDefault()
     panelUrl.style.display = 'none'
   })
+
+  function btnOpenFunc(url) {
+    btnOpen.setAttribute('href', url)
+    btnOpen.setAttribute('target', '_blank')
+  }
 
   function urlGenerate() {
 
@@ -21,10 +30,11 @@
     let inputUrl = document.querySelector('.url-generate')
     let url = `https://api.whatsapp.com/send?=+55${phone.value.replace(/\D/g,'')}phone&text=${msg.value.replace(/\s/g, '%20')}`
 
-    if(phone.value === '') {
-      alert('Phones field is empty!')
+    if(phone.value === '' || msg.value === '') {
+      alert('Has fields empty!')
     }
     else {
+      btnOpenFunc(url)
       panelUrl.style.display = ''
       return inputUrl.value = url
     }
