15e150be1f244acf9b6c6c5a9891b79ba815ed2a
leandromac <contato@leandromacedo.com>
2020-09-18 21:32:22 -0300

    Include notifications with jquery

--- Day-5/js/script.js
+++ Day-5/js/script.js
@@ -1,13 +1,14 @@
 ;{
   'use strict'
 
+  $('.generated-notification').hide(0)
+  $('.copied-notification').hide(0)
   const btn = document.querySelector('.btn')
   const btnDelete = document.querySelector('[aria-label="delete"]')
   const panelUrl = document.querySelector('.message')
   const btnCopy = document.querySelector('.btn-copy')
   const btnOpen = document.querySelector('.btn-open')
   const inputUrl = document.querySelector('.url-generate')
-
 
   btn.addEventListener('click', e => {
     e.preventDefault()
@@ -20,6 +21,8 @@
   })
 
   btnCopy.addEventListener('click', e => {
+    $('.copied-notification').show(1000)
+    setInterval( function() { $('.copied-notification').hide(1000) }, 5000)
     inputUrl.select()
     inputUrl.setSelectionRange(0, 99999)
     document.execCommand('copy')
@@ -32,6 +35,8 @@
   }
 
   function urlGenerate() {
+    $('.generated-notification').show(1000)
+    setInterval( function() { $('.generated-notification').hide(1000) }, 5000)
     let phone = document.querySelector('.phone')
     let msg = document.querySelector('.msg')
     let url = `https://api.whatsapp.com/send?phone=+55${phone.value.replace(/\D/g,'')}&text=${msg.value.replace(/\s/g, '%20')}`
