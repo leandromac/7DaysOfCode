c479d28d71628b4bf6b3fd8f2661bd12388d5aa2
leandromac <contato@leandromacedo.com>
2020-09-19 16:08:33 -0300

    Finished project version 1

--- Day-6/script.js
+++ Day-6/script.js
@@ -1,21 +1,46 @@
-const timeInterval = 5
-const duration = 4000
+;{
+  'use strict'
 
-$('.counter-up').each(function() {  
-  let count_to = parseInt($(this).data('countTo'))
-  let interval = duration / timeInterval
-  let incremento = count_to / interval
-  let num = 0
-  let el = $(this)
-  
-  let timer = setInterval(function() {
-    if (num >= count_to){
-      num = count_to;
-      clearInterval(timer);
-    }
-    
-    let txt = num.toFixed(0).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.")
-    el.text(txt)
-    num += incremento      
-  }, timeInterval)
-})
+  const rand = (minimum, maximum) => {
+    const min = Math.ceil(minimum)
+    const max = Math.floor(maximum)
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+  const btnRandom = document.querySelector('.btn-random')
+  const startNumber = document.querySelector('.start-number')
+  const endNumber = document.querySelector('.end-number')
+  const counterUp = document.querySelector('.counter-up')
+
+  btnRandom.addEventListener('click', e => {
+    e.preventDefault()
+    animationNumber()
+  })
+
+  function animationNumber() {
+    const timeInterval = 5
+    const duration = 1000
+    let numberSorted = rand(startNumber.value, endNumber.value)
+    counterUp.setAttribute('data-count-to', numberSorted)
+    console.log(numberSorted)
+
+    $('.counter-up').each(function() {  
+      let count_to = parseInt(numberSorted)
+      let interval = duration / timeInterval
+      let incremento = count_to / interval
+      let num = 0
+      let el = $(this)
+      
+      let timer = setInterval(function() {
+        if (num >= count_to){
+          num = count_to;
+          clearInterval(timer);
+        }
+        
+        let txt = num.toFixed(0).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.")
+        el.text(txt)
+        num += incremento      
+      }, timeInterval)
+    })
+  }
+}
