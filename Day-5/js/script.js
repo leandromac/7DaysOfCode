e853ec86b00f54f252c707112ceb7f7a273a1af6
leandromac <contato@leandromacedo.com>
2020-09-21 09:44:42 -0300

    Refatoring project Day 5

--- Day-5/js/script.js
+++ Day-5/js/script.js
@@ -1,34 +1,27 @@
 ;{
   'use strict'
 
-  const btn = document.querySelector('.btn')
-  const btnDelete = document.querySelector('[aria-label="delete"]')
-  const panelUrl = document.querySelector('.message')
-  const btnCopy = document.querySelector('.btn-copy')
-  const btnOpen = document.querySelector('.btn-open')
   const inputUrl = document.querySelector('.url-generate')
 
-  btn.addEventListener('click', e => {
+  $('.btn').click(function(e) {
     e.preventDefault()
     urlGenerate()
   })
 
-  btnDelete.addEventListener('click', e => {
+  $('[aria-label="delete"]').click(function(e) {
     e.preventDefault()
     $('.message').fadeOut(800)
   })
 
-  btnCopy.addEventListener('click', e => {
+  $('.btn-copy').click(function(e) {
     $('.copied-notification').show(1000)
     setInterval( function() { $('.copied-notification').hide(1000) }, 5000)
     inputUrl.select()
     inputUrl.setSelectionRange(0, 99999)
     document.execCommand('copy')
-    
   })
 
   function notification() {
-
     if(!$('.phone').val() === true || !$('.msg').val() === true) {
       $('.empty-notification').show(1000)
       setInterval( function() { $('.empty-notification').hide(1000) }, 5000)
@@ -37,31 +30,22 @@
       $('.generated-notification').show(1000)
       setInterval( function() { $('.generated-notification').hide(1000) }, 5000)
     }
-    if($('.btn-copy').click()) {
-      $('.copied-notification').show(1000)
-      setInterval( function() { $('.copied-notification').hide(1000) }, 5000)
-    }
   }
 
   function btnOpenFunc(url) {
-    btnOpen.setAttribute('href', url)
-    btnOpen.setAttribute('target', '_blank')
+    $('.btn-open').attr('href', url).attr('target', '_blank')
   }
 
   function urlGenerate() {
-    let phone = document.querySelector('.phone')
-    let msg = document.querySelector('.msg')
-    let url = `https://api.whatsapp.com/send?phone=+55${phone.value.replace(/\D/g,'')}&text=${msg.value.replace(/\s/g, '%20')}`
-    
-    if(phone.value === '' || msg.value === '') {
-      $('.empty-notification').show(1000)
-      setInterval( function() { $('.empty-notification').hide(1000) }, 5000)
+    let url = `https://api.whatsapp.com/send?phone=+55${$('.phone').val().replace(/\D/g,'')}&text=${$('.msg').val().replace(/\s/g, '%20')}`
+
+    if($('.phone').val() === '' || $('.msg').val() === '') {
+      notification()
     }
     else {
-      $('.generated-notification').show(1000)
-      setInterval( function() { $('.generated-notification').hide(1000) }, 5000)
+      notification()
       btnOpenFunc(url)
-      panelUrl.style.display = ''
+      $('.message').css('display', '')
       return inputUrl.value = url
     }
   }
