56fe4e1eb839600d5def2ab30e61c5fbab654d76
leandromac <contato@leandromacedo.com>
2020-09-21 10:40:10 -0300

    Refatoring

--- Day-1/js/script.js
+++ Day-1/js/script.js
@@ -1,25 +1,28 @@
+'use strict'
 
-  const input = document.querySelector('input')
-  const buttonAdd = document.querySelector('.button')
-  const list = document.querySelector('.list')
+const input = document.querySelector('input')
+const buttonAdd = document.querySelector('.button')
+const list = document.querySelector('.list')
 
-  buttonAdd.addEventListener('click', e => {
-    if(!!input.value === false) {
-      alert('You need to type a task!')
-    }
-    else {
-      let a = document.createElement('a')
-      a.className = "panel-block task is-active"
-      a.innerHTML = `<span class="panel-icon">
-                      <i class="fas fa-book" aria-hidden="true"></i>
-                    </span>
-                    ${input.value}`
-      a.setAttribute('onclick', 'taskDone(this)')
-      list.appendChild(a)
-      input.value = ''
-    }
-  })
-         
-  function taskDone(element) {
-    element.classList.add('has-text-success')
+buttonAdd.addEventListener('click', e => {
+  if(!!input.value === false) {
+    alert('You need to type a task!')
   }
+  else {
+    let a = document.createElement('a')
+    a.className = "panel-block task is-active"
+    a.innerHTML = `<span class="panel-icon">
+                    <i class="fas fa-book" aria-hidden="true"></i>
+                  </span>
+                  ${input.value}`
+    a.setAttribute('onclick', 'taskDone(this)')
+    list.appendChild(a)
+    input.value = ''
+  }
+})
+        
+function taskDone(element) {
+  element.classList.add('has-text-success')
+  element.style.textDecoration = 'line-through'
+  element.style.opacity = '0.4'
+}
--- Day-3/src/js/api.cep.js
+++ Day-3/src/js/api.cep.js
@@ -1,32 +1,37 @@
-const inputCEP = document.querySelector('.input')
-const buttonBuscarCEP = document.querySelector('.button')
+(function(){
+  'use strict'
 
-buttonBuscarCEP.addEventListener("click", e => {
-  let cep = inputCEP.value.replace("-","")
-  
-  const options = {
-    method: 'GET',
-    mode: 'cors',
-    cache: 'default'
-  }
+  const inputCEP = document.querySelector('.input')
+  const buttonBuscarCEP = document.querySelector('.button')
 
-  fetch(`https://viacep.com.br/ws/${cep}/json/`)
-  .then(response => { response.json()
-    .then( data => {
-      
-      let result = document.querySelector('.result-text')
-      result.innerHTML = `Resultado da pesquisa para o CEP <strong>${data.cep}</strong>`
-      result.style.display = ''
+  buttonBuscarCEP.addEventListener("click", e => {
+    let cep = inputCEP.value.replace("-","")
+    
+    const options = {
+      method: 'GET',
+      mode: 'cors',
+      cache: 'default'
+    }
 
-      let cep = document.querySelector('.cep').innerText = data.cep
-      let logradouro = document.querySelector('.logradouro').innerText = data.logradouro
-      let bairro = document.querySelector('.bairro').innerText = data.bairro
-      let localidade = document.querySelector('.localidade').innerText = data.localidade
-      let uf = document.querySelector('.uf').innerText = data.uf
-      let ddd = document.querySelector('.ddd').innerText = data.ddd
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(response => { response.json()
+      .then( data => {
+        
+        let result = document.querySelector('.result-text')
+        result.innerHTML = `Resultado da pesquisa para o CEP <strong>${data.cep}</strong>`
+        result.style.display = ''
 
-    } )
+        let cep = document.querySelector('.cep').innerText = data.cep
+        let logradouro = document.querySelector('.logradouro').innerText = data.logradouro
+        let bairro = document.querySelector('.bairro').innerText = data.bairro
+        let localidade = document.querySelector('.localidade').innerText = data.localidade
+        let uf = document.querySelector('.uf').innerText = data.uf
+        let ddd = document.querySelector('.ddd').innerText = data.ddd
+
+      } )
+    })
+    .catch( e => alert('Desculpe, este CEP não foi encontrado!') )
+
   })
-  .catch( e => alert('Desculpe, este CEP não foi encontrado!') )
 
-})
+})()
